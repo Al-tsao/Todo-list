86e70837cbb37d27e3e83cf4e1e38cc27ca29b16
AlTsao <al.tsao.yw@gmail.com>
2020-11-12 13:15:18 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 const exphbs = require('express-handlebars');
 const Todo = require('./models/todo') // 載入 Todo model
+const bodyParser = require('body-parser')
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -17,6 +18,10 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
@@ -25,8 +30,16 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error)) // 錯誤處理
 })
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
 
 // 設定 port 3000
 app.listen(3000, () => {
